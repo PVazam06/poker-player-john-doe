6d985ccb86c802b239ecaea4cd2da3964eb4c8fe
VMWIN10\admin <julianf434@gmail.com>
2017-10-14 10:16:27 +0200

    info log

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,9 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 5;
+    	String str = request.getAsString();
+        System.out.println(str);
+    	return 5;
     }
 
     public static void showdown(JsonElement game) {
