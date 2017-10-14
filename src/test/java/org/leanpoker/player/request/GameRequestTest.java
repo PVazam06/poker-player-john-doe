701ee875a4f240c378e980d03216e99a5aee799b
Mathias Bottler <mathias.bottler@gmx.de>
2017-10-14 11:12:48 +0200

    GameRequest, PlayerJson Grundgerüst

---
+++ src/test/java/org/leanpoker/player/request/GameRequestTest.java
@@ -0,0 +1,14 @@
+package org.leanpoker.player.request;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameRequestTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
